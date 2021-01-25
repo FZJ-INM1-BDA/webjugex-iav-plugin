b1d79986e2f6b3ae36e12e152bbad2705fb283c8
Xiao Gui <xgui3783@gmail.com>
2021-01-25 17:24:09 +0100

    bugfix: region replace in notebook

--- deploy/user/util.js
+++ deploy/user/util.js
@@ -16,11 +16,13 @@ const getAreas = arr => JSON.stringify(JSON.stringify(arr))
 const getAreasFromStringV2 = value => {
   const arr = JSON.parse(value)
   return arr.map(v => {
-    const re = /(^.*?)\s-\s((left|right)\shemisphere)$/.exec(v)
-    return re && {
-      name: re[1],
-      hemisphere: re[2]
-    }
+    const re = /(^.*?)\s-\s((left|right)\shemisphere)$/.exec(v.name)
+    return re
+    ? {
+        name: re[1],
+        hemisphere: re[2],
+      }
+    : { name: v.name, hemisphere: v.status }
   })
 } 
 
@@ -48,8 +50,8 @@ const getNBFromPostReq = ({ body }) => {
     if(!body[key]) throw new Error(`body[${key}] needs to be defined`)
   }
 
-  const areas1 = getAreasFromStringV2(body['webjugex-analysis-roi1'])
-  const areas2 = getAreasFromStringV2(body['webjugex-analysis-roi2'])
+  const areas1 = getAreasFromStringV2(body['webjugex-analysis-roi1']).map(v => v && `${v.name} - ${v.hemisphere}`)
+  const areas2 = getAreasFromStringV2(body['webjugex-analysis-roi2']).map(v => v && `${v.name} - ${v.hemisphere}`)
   const threshold = Number(body['webjugex-analysis-threshold'])
   const rep = Number(body['webjugex-analysis-permutations'])
 
@@ -57,8 +59,8 @@ const getNBFromPostReq = ({ body }) => {
     .replace(/^\"/, '')
     .replace(/\"$/, '')
 
-  const area1Replace = getAreas(areas1)
-  const area2Replace = getAreas(areas2)
+  const area1Replace = getAreas(areas1[0])
+  const area2Replace = getAreas(areas2[0])
   
   return template
     .replace(/\$AREA1\$/g, area1Replace)
